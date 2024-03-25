9b177c96aa0df623547962cc433c2013e0d20ad8
Lilall5829 <lilall5829@gmail.com>
2024-03-25 00:23:56 -0400

    Typescript of database

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 //@ represent the root directory: FoodOrdering
 import Colors from "@constants/Colors";
-import { Product } from "@/types";
+import { Tables } from "@/types";
 import { Link, useSegments } from "expo-router";
 
 type ProductListItemProps = {
-  product: Product;
+  product: Tables<"products">;
 };
 export const defaultPizzaImage =
   "https://p16-flow-sign-va.ciciai.com/ocean-cloud-tos-us/ed68cf0e763e4993bc8efa195cf0f133.png~tplv-6bxrjdptv7-image.png?rk3s=18ea6f23&x-expires=1739939558&x-signature=%2B9rFwlDL26cRNwOclICZQ823%2BNU%3D";
