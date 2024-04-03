247c4573b041da8d0ca9b48997c2c74814fa4a61
Lilall5829 <lilall5829@gmail.com>
2024-04-03 17:27:32 -0400

    database initial

--- src/app/(user)/profile.tsx
+++ src/app/(user)/profile.tsx
@@ -1,12 +1,13 @@
 import { supabase } from "@/lib/supabase";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
+import Button from "@components/Button";
 
 const ProfileScreen = () => {
   return (
     <View>
       {/* <Text>ProfileScreen</Text> */}
       <Button
-        title="Sign out"
+        text="Sign out"
         onPress={async () => await supabase.auth.signOut()}
       />
     </View>
